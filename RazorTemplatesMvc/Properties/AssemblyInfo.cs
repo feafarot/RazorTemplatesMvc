252a57ca1f6630ed990e85fee98947a91f925c9b
feafarot <sly.feafarot@gmail.com>
2012-09-13 17:11:08 +0300

    Added autoincrement revision version.

--- RazorTemplatesMvc/Properties/AssemblyInfo.cs
+++ RazorTemplatesMvc/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("RazorTemplatesMvc")]
 [assembly: AssemblyDescription("Custom razor files templates")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("RazorTemplatesMvc")]
 [assembly: AssemblyCopyright("feafarot")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.*")]
