82cc51854d9cbac0a2003f010288e480e67c3423
feafarot <sly.feafarot@gmail.com>
2012-09-24 12:35:34 +0300

    Migrated solution to VS2012.

---
+++ Backup/RazorTemplatesMvc.Sample/Global.asax.cs
@@ -0,0 +1,29 @@
+﻿namespace RazorTemplatesMvc.Sample
+{
+    using System.Web.Mvc;
+    using System.Web.Routing;
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            var templateRoute = TemplateRouteFactory.CreateRoute("css/{cssname}.css", ContentType.Css, "~/Content", FileTypeHandlingMode.WithCsExtensionPrefix);
+            routes.Add("CssRoute", templateRoute); 
+            routes.Add("JsRoute", TemplateRouteFactory.CreateRoute("javascript/{jsname}.js", ContentType.Js, "~/Scripts", FileTypeHandlingMode.TransformFromCshtml));
+            routes.MapRoute("Default", "{controller}/{action}/{id}", new { controller = "Home", action = "Index", id = UrlParameter.Optional });
+        }
+
+        protected void Application_Start()
+        {            
+            AreaRegistration.RegisterAllAreas();
+            RegisterGlobalFilters(GlobalFilters.Filters);
+            RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
